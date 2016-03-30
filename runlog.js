534f1cfd986296069daf2c49fc06ae3c45c477c2
dkmullen <dkmullen@gmail.com>
2016-03-29 22:51:21 -0400

    feat: Work on logging function

--- runlog.js
+++ runlog.js
@@ -6,7 +6,20 @@ function ViewModel() {
 		if (hours == undefined) {
 			hours = 0;
 		}
-		console.log(date, miles, hours, minutes, seconds, comments());
+		var totalSeconds = seconds + (minutes * 60) + (hours * 3600);
+		console.log(seconds, (minutes * 60), hours);
+		console.log(totalSeconds);
+		var secPerMile = totalSeconds / miles;
+		console.log(secPerMile);
+		var paceMinutes = Math.floor(secPerMile / 60);
+		console.log(paceMinutes);
+		var paceSeconds = ((secPerMile / 60) - paceMinutes) * 60;
+		
+		document.getElementById('date').innerHTML = date;
+		document.getElementById('miles').innerHTML = miles;
+		document.getElementById('time').innerHTML = hours + ':' + minutes +
+			':' + seconds;
+		document.getElementById('pace').innerHTML = paceMinutes + ':' + paceSeconds;
 	}
 }
 ko.applyBindings(new ViewModel());
