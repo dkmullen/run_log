17f09156b0f8d8a1816cc4433dcc984eaf6c6a1b
dkmullen <dkmullen@gmail.com>
2016-05-24 21:12:10 -0400

    feat: Add error message, fix the use of pad2 function in adding zeros, add checkEntry function

--- runlog.js
+++ runlog.js
@@ -4,25 +4,48 @@ var hours = ko.observable();
 var minutes = ko.observable();
 var seconds = ko.observable();
 var comments = ko.observable();
+var errorMessage = ko.observable(false);
 
 function ViewModel() {
 	
 	this.pad2 = function(number) {
-		return (number < 10 ? '0' : '') + number
-	}
+		return (number < 10 ? '0' : '') + number;
+	};
+	
+	this.checkEntry = function() {
+		if (miles() === undefined) {
+			errorMessage(true);
+		}
+		else if (hours() === undefined && minutes() === undefined) {
+			errorMessage(true);
+		}
+		else {
+			this.logMyRun();
+		}
+	};
 	
 	this.logMyRun = function() {
+		errorMessage(false);
 		if (date() === undefined) {
 			date('');
 		}
-		if (hours() === undefined) {
+		if (hours() !== undefined && minutes() === undefined) {
+			minutes('00');
+		}
+		if (hours() === undefined && minutes() !== undefined) {
 			hours('0');
 		}
 		if (seconds() === undefined) {
-			seconds('0');
+			seconds('00');
 		}
 		if (comments() === undefined) {
 			comments('');
+		}
+		if (minutes().length < 2) {
+			minutes(this.pad2(minutes())) 
+		}
+		if (seconds().length < 2) {
+			seconds(this.pad2(seconds())) 
 		}
 		var totalSeconds = parseInt(seconds()) + (parseInt(minutes()) * 60) + 
 			(parseInt(hours()) * 3600);
@@ -33,11 +56,21 @@ function ViewModel() {
 		document.getElementById('date').innerHTML = date();
 		document.getElementById('miles').innerHTML = miles();
 		document.getElementById('time').innerHTML = hours() + ':' + 
-			this.pad2(minutes()) + ':' + this.pad2(seconds());
+			minutes() + ':' + seconds();
 		document.getElementById('pace').innerHTML = this.pad2(paceMinutes) + 
 			':' + this.pad2(paceSeconds);
 		document.getElementById('comments').innerHTML = comments();
-	}
+	};
+	
+	this.formReset = function() {
+		miles(undefined);
+		hours(undefined);
+		minutes(undefined);
+		seconds(undefined);
+		comments(undefined);
+		errorMessage(false);
+		document.getElementById('run-log-form').reset();
+	};
 }
 ko.applyBindings(new ViewModel());
 
