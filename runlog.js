9cdbb8a23cfc822f711fbd7c6f4dcd9b0ae23699
dkmullen <dkmullen@gmail.com>
2016-05-28 17:03:26 -0400

    debug: Fix error when seconds reaches sixty; break out new function for filling in data

--- runlog.js
+++ runlog.js
@@ -23,7 +23,7 @@ function ViewModel() {
 	/**
 	 * Function called directly by 'Submit' button. Checks to see if needed
 	 * fields are filled out. If not, flips the errorMessage observable to
-	 * true. Otherwise called the main function of the app, logMyRun.
+	 * true. Otherwise called the next function of the app, fillInData.
 	 * @function
 	 */	
 	this.checkEntry = function() {
@@ -34,19 +34,17 @@ function ViewModel() {
 			errorMessage(true);
 		}
 		else {
-			this.logMyRun();
+			this.fillInData();
 		}
 	};
 
 	/**
-	 * Main function of the app. Fills in with zeros any optional fields left
-	 * blank, calls pad2 to add leading zeros to some single-digit fields, 
-	 * calculates the pace of the run and 'logs' it by adding data to the DOM.
-	 * Calls formReset to clear the form and toggles the observable showTable
-	 * to 'true' to display the previously hidden data div.
+	 * A function that fills in with zeros any optional fields left
+	 * blank, calls pad2 to add leading zeros to some single-digit fields,
+	 * then calls logMyRun;
 	 * @function
 	 */	
-	this.logMyRun = function() {
+	this.fillInData = function() {
 		/** Turns off error message in case it was on from a previous attempt.*/
 		errorMessage(false);
 		if (date() === undefined) {
@@ -66,31 +64,49 @@ function ViewModel() {
 		}
 		//Checks for single digit numbers, adds a leading zero with pad2.
 		if (minutes().length < 2) {
-			minutes(this.pad2(minutes())) 
+			minutes(this.pad2(minutes())); 
 		}
 		if (seconds().length < 2) {
-			seconds(this.pad2(seconds())) 
+			seconds(this.pad2(seconds())); 
 		}
+		this.logMyRun();
+	};
+		
+		 
+	/** 
+	 * Calculates the pace of the run and 'logs' it by adding data to the DOM.
+	 * Calls formReset to clear the form and toggles the observable showTable
+	 * to 'true' to display the previously hidden data div.
+	 * @function
+     */
+	  
+	this.logMyRun = function() {
 		//parseInt because numbers from the form are delivered as strings
 		var totalSeconds = parseInt(seconds()) + (parseInt(minutes()) * 60) + 
 			(parseInt(hours()) * 3600);
 		var secPerMile = totalSeconds / parseFloat(miles());
 		//Math.floor finds all the complete minutes, drops remaining seconds
-		var paceMinutes = Math.floor(secPerMile / 60);
+		var paceMinutes = this.pad2(Math.floor(secPerMile / 60));
 		//Finds the remaining seconds from above, rounds to the nearest sec.
-		var paceSeconds = Math.round(((secPerMile / 60) - paceMinutes) * 60);
+		var paceSeconds = this.pad2(Math.round(((secPerMile / 60) - paceMinutes) * 60));
+		if (paceSeconds === '60') {
+			//parseInt because pad2 returns a string
+			paceMinutes = parseInt(paceMinutes) + 1;
+			paceSeconds = '00';
+		}
 		
 		document.getElementById('date').innerHTML = date();
 		document.getElementById('miles').innerHTML = miles();
 		document.getElementById('time').innerHTML = hours() + ':' + 
 			minutes() + ':' + seconds();
-		document.getElementById('pace').innerHTML = this.pad2(paceMinutes) + 
-			':' + this.pad2(paceSeconds);
+		document.getElementById('pace').innerHTML = paceMinutes + ':' + 
+			paceSeconds;
 		document.getElementById('comments').innerHTML = comments();
 		
 		this.formReset();
 		showTable(true);
 	};
+	
 
 	/**
 	 * Empties the form; Called when Submit button is hit
