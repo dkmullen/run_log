d5ec8bf4936282374e9d6d54b988ac22e8def3b9
Dennis Mullen <dkmullen@gmail.com>
2017-09-11 21:21:44 -0400

    Begin work on db connection

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 const uri = 'mongodb://localhost:27017/runlog';
 
 module.exports = {
-    'secret': 'fakesecret',
+    //'secret': 'fakesecret',
     'mongoUrl' : uri
 };
 // To start mongo on Windows - "C:\Program Files\MongoDB\Server\3.4\bin\mongod.exe"
--- controllers/run_controller.js
+++ controllers/run_controller.js
@@ -5,12 +5,13 @@ const express = require('express'),
   config = require('../config'),
   Run = require('../models/run');
 
-app.set('secretKey', config.secret);
+//app.set('secretKey', config.secret);
 
 module.exports = {
   // Create a new record
   createrun(req, res, next) {
     const newRun = req.body; // const = entire request body sent in
+    console.log(newRun);
     Run.create(newRun) // create a new run record out of the const
       .then(run => res.send(run))
       .catch(next);
