73f88c7506956c773105335071eaf6752eaad51a
Dennis Mullen <dkmullen@gmail.com>
2017-09-22 20:08:39 -0400

    Solve connection to db

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /*jshint esversion: 6 */
 
-const uri = 'mongodb://dkmullen:rl456%@ds147304.mlab.com:47304/runlog';
+const uri = 'mongodb://dkm2:songnva@ds147304.mlab.com:47304/runlog';
 const uri2 = 'mongodb://localhost:27017/runlog';
 
 module.exports = {
